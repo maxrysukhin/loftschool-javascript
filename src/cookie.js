350e576b6909fc12d80fadf010de10779b35019b
Max Rysukhyn <m.mowgly@gmail.com>
2018-12-01 13:18:54 +0200

    HW7. Wroks but does not pass tests

---
+++ src/cookie.js
@@ -0,0 +1,135 @@
+/*
+ ДЗ 7 - Создать редактор cookie с возможностью фильтрации
+
+ 7.1: На странице должна быть таблица со списком имеющихся cookie. Таблица должна иметь следующие столбцы:
+   - имя
+   - значение
+   - удалить (при нажатии на кнопку, выбранная cookie удаляется из браузера и таблицы)
+
+ 7.2: На странице должна быть форма для добавления новой cookie. Форма должна содержать следующие поля:
+   - имя
+   - значение
+   - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+
+ Если добавляется cookie с именем уже существующией cookie, то ее значение в браузере и таблице должно быть обновлено
+
+ 7.3: На странице должно быть текстовое поле для фильтрации cookie
+ В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
+ Если в поле фильтра пусто, то должны выводиться все доступные cookie
+ Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
+ то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+// текстовое поле для фильтрации cookie
+const filterNameInput = homeworkContainer.querySelector('#filter-name-input');
+// текстовое поле с именем cookie
+const addNameInput = homeworkContainer.querySelector('#add-name-input');
+// текстовое поле со значением cookie
+const addValueInput = homeworkContainer.querySelector('#add-value-input');
+// кнопка "добавить cookie"
+const addButton = homeworkContainer.querySelector('#add-button');
+// таблица со списком cookie
+const listTable = homeworkContainer.querySelector('#list-table tbody');
+
+fillInTable();
+
+filterNameInput.addEventListener('keyup', function() {
+    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+    fillInTable();
+});
+
+listTable.addEventListener('click', (e) => {
+    if (e.target.innerHTML === 'удалить') {
+        const cookieToDelete = e.target.parentElement.firstElementChild.innerHTML;
+        const date = new Date();
+
+        date.setTime(date.getTime()-(24*60*60*1000));
+        document.cookie=`${cookieToDelete}=; expires=${date.toGMTString()}`;
+
+        fillInTable();
+    }
+});
+
+addButton.addEventListener('click', () => {
+    // здесь можно обработать нажатие на кнопку "добавить cookie"
+
+    if (addNameInput.value !== '' && addValueInput.value !== '') {
+        document.cookie = `${addNameInput.value}=${addValueInput.value}`
+
+        addNameInput.value = '';
+        addValueInput.value = '';
+    }
+        
+    fillInTable();
+});
+
+function getCookies() {
+    
+    if (document.cookie !== '') {
+        return document.cookie.split('; ').reduce((prev, curr) => {
+            const [name, val] = curr.split('=');
+
+            prev[name] = val;
+
+            return prev;
+        }, {})
+    }
+
+    return '';
+}
+
+function isMatching(chunk, full) {
+    let pattern = new RegExp(chunk, 'i');
+
+    if (!chunk) {
+        return true;
+    }
+
+    for (const word of full) {
+        if (pattern.test(word)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function fillInTable() {
+    const cookie = getCookies();
+    const filter = filterNameInput.value;
+
+    listTable.innerHTML = '';
+
+    for (let x of Object.getOwnPropertyNames(cookie)) {
+        if (isMatching(filter, [x, cookie[x]])) {
+            
+            const tableRow = document.createElement('tr');
+
+            const name = document.createElement('td');
+            const val = document.createElement('td');
+            const del = document.createElement('td');
+
+            name.innerHTML = x;
+            val.innerHTML = cookie[x];
+            del.innerHTML = 'удалить';
+
+            tableRow.appendChild(name);
+            tableRow.appendChild(val);
+            tableRow.appendChild(del);
+
+            listTable.appendChild(tableRow);
+        }
+    }
+}
